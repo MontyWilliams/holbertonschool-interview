2b089d359b2704b91c73c8db791451e99566fe82
Montywilliams <3650@holbertonschool.com>
2022-10-20 22:14:44 -0600

    linked lisr

---
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - check if a singly linked
+ * list is or is not a palindrome
+ * @head: double pointer to the head (node 0) of the list
+ * Return: 1 or 0
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int count = 0;
+	int i = 0;
+	listint_t *mover;
+	int *listn;
+
+	if (!head)
+		return (0);
+	if (!*head)
+		return (1);
+
+	mover = *head;
+	if (mover->next == NULL)
+		return (1);
+
+	while (mover != NULL)
+	{
+		count++;
+		mover = mover->next;
+	}
+	listn = malloc(sizeof(int) * count);
+	if (listn == NULL)
+		return (0);
+	mover = *head;
+	while (mover != NULL)
+	{
+		listn[i] = mover->n;
+		i++;
+		mover = mover->next;
+	}
+	mover = *head;
+	while (mover != NULL)
+	{
+		if (listn[i - 1] == mover->n)
+		{
+			i--;
+			mover = mover->next;
+		}
+		else
+			return (0);
+	}
+	return (1);
+}
